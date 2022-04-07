a17c943669c19e6ad2df47082140724b1a1ce5bf
root <ngugijamesw@gmail.com>
2022-04-06 20:24:01 -0400

    hot fix 3

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - prints a string in stdout
- * @str: string to print
- *
- * Return: void
+ * _puts - print a string
+ * @str: string
  */
+
 void _puts(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (str[i] != '\0')
 	{
-		_putchar(*(str + i));
-		i++;
+		_putchar(str[i]);
+		i += 1;
 	}
 	_putchar('\n');
 }
